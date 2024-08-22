efd38ff4e7a06ec75bc9c9568b50c564d306f334
maturan <janbrian630@gmail.com>
2024-08-22 12:02:03 +0800

    Initial commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ F_num = int(input("Enter first number: "))
 S_num = int(input("Enter second number: "))
 
 if F_num < S_num:
-    print(F_num + " is less than " + S_num)
+    print(f"{ F_num } is less than { S_num }")
 
 elif F_num > S_num:
-    print(F_num + " is greater than " + S_num)
+    print(f"{ F_num } is greater than { S_num }")
 
 else:
-    print(F_num + " is equal to " + S_num)
+    print(f"{ F_num } is equal to { S_num }")
 
 
 # name1 = input("Enter classmate name 1: ")
