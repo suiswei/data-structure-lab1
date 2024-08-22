fc733a2c3ad12492a199b29a698e823f7678fdce
maturan <janbrian630@gmail.com>
2024-08-22 12:04:58 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ else:
     print(f"{ F_num } is equal to { S_num }")
 
 
-# name1 = input("Enter classmate name 1: ")
-# name2 = input("Enter classmate name 2: ")
-# name3 = input("Enter classmate name 3: ")
-#
-# print("Your classmates are: " + name1 + ", " + name2 + ", & " + name3)
+name1 = input("Enter classmate name 1: ")
+name2 = input("Enter classmate name 2: ")
+name3 = input("Enter classmate name 3: ")
+
+print("Your classmates are: " + name1 + ", " + name2 + ", & " + name3)
 
