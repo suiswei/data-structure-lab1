1793aaca31f285e955b2405f66c4ee3c965febea
maturan <janbrian630@gmail.com>
2024-08-22 11:49:47 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+F_num = int(input("Enter first number: "))
+S_num = int(input("Enter second number: "))
+
+if F_num < S_num:
+    print(F_num + " is less than " + S_num)
+
+elif F_num > S_num:
+    print(F_num + " is greater than " + S_num)
+
+else:
+    print(F_num + " is equal to " + S_num)
+
+
+# name1 = input("Enter classmate name 1: ")
+# name2 = input("Enter classmate name 2: ")
+# name3 = input("Enter classmate name 3: ")
+#
+# print("Your classmates are: " + name1 + ", " + name2 + ", & " + name3)
+
